b5185747ea5abf50c1dbaebdf268d7f264ceb9ec
ChanJair <chan.jair@gmail.com>
2018-05-07 10:09:23 -0500

    examen con toggle

---
+++ js/Examen.js
@@ -0,0 +1,36 @@
+function ocultar(){
+    document.getElementById("capa").classList.toggle("invisible");
+}
+function ocultar1(){
+    document.getElementById("capa1").classList.toggle("invisible");
+}
+function ocultar2(){
+    document.getElementById("capa2").classList.toggle("invisible");
+}
+function ocultar3(){
+    document.getElementById("capa3").classList.toggle("invisible");
+}
+function ocultar4(){
+    document.getElementById("capa4").classList.toggle("invisible");
+}
+function ocultar5(){
+    document.getElementById("capa5").classList.toggle("invisible");
+}
+function ocultar6(){
+    document.getElementById("capa6").classList.toggle("invisible");
+}
+function ocultar7(){
+    document.getElementById("capa7").classList.toggle("invisible");
+}
+function ocultar8(){
+    document.getElementById("capa8").classList.toggle("invisible");
+}
+function ocultar9(){
+    document.getElementById("capa9").classList.toggle("invisible");
+}
+function ocultar10(){
+    document.getElementById("capa10").classList.toggle("invisible");
+}
+function ocultar11(){
+    document.getElementById("capa11").classList.toggle("invisible");
+}
